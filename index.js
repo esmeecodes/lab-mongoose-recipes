728c416581caa1d17713c6dc9343a2a9c4b19638
Esmée Zootjes <esmeezootjes@gmail.com>
2023-11-10 17:37:52 +0100

    done

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  })
+  // .then(() => {
+  //   Recipe.create(data[0]) // input is een array, daarom hiermet een [0] gewerkt, met create kan je maar 1 document/record tegelijk toevoegen
+  //     .then((result) => {
+  //       console.log(result.title);
+  //     })
+  //     .catch((error) => {
+  //       console.error("error loading recipe", error);
+  //     })
+  .then(() => {
+    return Recipe.insertMany(data) // hiermee kan je een array van data invoeren
+      .then((results) => {
+        results.forEach((recipe) => {
+          // hier ben ik in de array gegaan met een forEach loop en heb daarna op elk element in de array de insertMany toegepast.
+          console.log(`added: ${recipe}`);
+        });
+      })
+      .catch((error) => {
+        console.error("error loading many recipes", error);
+      });
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.findOneAndUpdate(
+      // return is nodig om het terug te geven aan de collectie
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => console.log("Error occured =>", error))
+  .then(() => {
+    Recipe.deleteOne({ title: "Carrot Cake" }).then((response) => {
+      console.log("carrot cake", response);
+      mongoose.connection.close();
+    });
+  })
+  .catch((error) => {
+    console.log("error has occurred while deleting");
   });
+
+// });
